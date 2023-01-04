153e864cb89777b63005decd7e20811949119e24
James Anziano <james.anziano@mr-h.life>
2023-01-04 10:27:17 -0600

    Process arguments regardless of order and various bugfixes

--- bulk_mkvpropedit.sh
+++ bulk_mkvpropedit.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
+
+# TODO Supply a list of default tracks and forced tracks (so it can just be --defaults 'a2,s1' --forced 's1'), etc.
 usage() {
 cat << EOF
 
     Usage: $0 <filepath> [options]
 
-    -h,  --help                 Display help
+For each <filepath> supplied, set the specified flags for the specified tracks.
+
+    -h,  --help                 Display help.
 
     -ad, --default-audio        Audio track to set as default. All other audio tracks
                                 will have their default flag switched off. Must be an
@@ -23,79 +28,92 @@ cat << EOF
 EOF
 }
 
-options=$(getopt -l "help,audio:,subtitle:,forced:" -o "h" -a "ad,sd,sf" -- "$@")
-
 default_audio_track=false
 default_subtitle_track=false
 forced_subtitle_track=false
 testing=false
 
-filepath="$1"
+positional_args=()
 
-while true; do
-    case "$2" in
+while [ "$1" != "" ]; do
+    case "$1" in
         -h|--help)
             usage
             exit 0
             ;;
         -ad|--default-audio)
-            shift
             default_audio_track="$2"
-            ;;
-        -sd|--default_subtitle)
             shift
+            ;;
+        -sd|--default-subtitle)
             default_subtitle_track="$2"
-            ;;
-        -sf|--forced_subtitle)
             shift
+            ;;
+        -sf|--forced-subtitle)
             forced_subtitle_track="$2"
+            shift
             ;;
         -t|--testing)
-            shift
             testing=true
             ;;
         --|*)
-            shift
-            break
+            if [[ "$1" =~ ^- ]]; then
+                echo "Unrecognized argument: $1"
+                exit 1
+            else
+                positional_args+=("$1")
+            fi
             ;;
     esac
     shift
 done
 
-if [[ ! -d "$filepath" ]]; then
-    echo "'$filepath' is not a directory or does not exist."
+if (( ${#positional_args[@]} < 1 )); then
+    echo "Please supply at least one filepath."
     exit 1
 fi
-if [[ ! $default_audio_track == false && ! $default_audio_track =~ ^[0-9]+$ ]]; then
+
+if [[ $default_audio_track != false && ! $default_audio_track =~ ^[0-9]+$ ]]; then
     echo "Default audio track '$default_audio_track' must be an integer."
     exit 1
 fi
-if [[ ! $default_subtitle_track == false && ! $default_subtitle_track =~ ^[0-9]+$ ]]; then
+if [[ $default_subtitle_track != false && ! $default_subtitle_track =~ ^[0-9]+$ ]]; then
     echo "Default subtitle track '$default_subtitle_track' must be an integer."
     exit 1
 fi
-if [[ ! $default_subtitle_track == false && ! $forced_subtitle_track =~ ^[0-9]+$ ]]; then
+if [[ $default_subtitle_track != false && ! $forced_subtitle_track =~ ^[0-9]+$ ]]; then
     echo "Forced subtitle track '$forced_subtitle_track' must be an integer."
     exit 1
 fi
 
-# TODO set these flags =0 for all other tracks
-command="find $filepath -name '*.mkv' -exec mkvpropedit {} "
-if [[ ! $default_audio_track == false ]]; then
-    command="$command --edit track:a$default_audio_track --set flag-default=1"
-fi
-if [[ ! $default_subtitle_track == false ]]; then
-    command="$command --edit track:s$default_subtitle_track --set flag-default=1"
-fi
-if [[ ! $default_audio_track == false ]]; then
-    command="$command --edit track:s$forced_subtitle_track --set flag-forced=1"
-fi
+for filepath in "${positional_args[@]}"; do
 
-command="$command \;"
-if [[ $testing == true ]]; then
-    echo "The command which would have been run:"
-    echo "$command"
-else
-    echo "Running $command"
-    eval "$command"
-fi
+    if [[ ! -d "$filepath" ]]; then
+        echo "'$filepath' is not a directory or does not exist."
+        exit 1
+    fi
+
+    # TODO set these flags =0 for all other tracks
+    # Need to find a way to determine number of other tracks. Maybe mkvinfo on the first file found by a similar `find` command?
+    command="find $filepath -name '*.mkv' -exec mkvpropedit {} "
+    if [[ ! $default_audio_track == false ]]; then
+        command="$command --edit track:a$default_audio_track --set flag-default=1"
+    fi
+    if [[ ! $default_subtitle_track == false ]]; then
+        command="$command --edit track:s$default_subtitle_track --set flag-default=1"
+    fi
+    if [[ ! $default_audio_track == false ]]; then
+        command="$command --edit track:s$forced_subtitle_track --set flag-forced=1"
+    fi
+
+    command="$command \;"
+    if [[ $testing == true ]]; then
+        echo "The command which would have been run:"
+        echo "$command"
+        echo ""
+    else
+        echo "Running $command"
+        eval "$command"
+        echo ""
+    fi
+done
